7df530a33d8a32b848731742a391892fbc63a171
RIFKY <rifkiardiansah96@gmail.com>
2024-10-26 21:15:59 +0700

    initial commit

--- app.js
+++ app.js
@@ -1,25 +1,97 @@
-const fs = require("node:fs")
-const readline = require('node:readline');
+const fs = require("node:fs");
+const path = require("node:path");
+const readline = require("node:readline");
 
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
 
-const app = {}
+const app = {};
 
-// contoh script pembuatan folder
- app.makeFolder = () => {
-    rl.question("Masukan Nama Folder : ",(folderName) => {
-        fs.mkdir(__dirname + `/${folderName}`,() => {
-            console.log("success created new folder");
-            
-        })
-        rl.close()
-    })
-} 
+// script make folder
+app.makeFolder = () => {
+  rl.question("Masukan Nama Folder : ", (folderName) => {
+    fs.mkdir(path.join(__dirname, folderName), (err) => {
+      if (err) return console.error("Failed make Folder:", err);
+      console.log("success created new folder :", folderName);
+      rl.close();
+    });
+  });
+};
 
-// To Do : lanjutkan pembuatan logic disini 
+// script make file
+app.makeFile = () => {
+  rl.question("Masukan Nama File beserta ekstensinya : ", (fileName) => {
+    fs.writeFile(path.join(__dirname, fileName), "", (err) => {
+      if (err) return console.error("Failed make File:", err);
+      console.log("success created new file :", fileName);
+      rl.close();
+    });
+  });
+};
 
+// script sort file
+app.extSorter = () => {
+  const sourceDir = path.join(__dirname, "unorganize_folder");
+  fs.readdir(sourceDir, (err, files) => {
+    if (err) return console.error("Failed Read Folder", err);
 
-module.exports = app
+    files.forEach((file) => {
+      const ext = path.extname(file).substring(1); // get file extention
+      const destDir = path.join(__dirname, ext);
+
+      if (!fs.existsSync(destDir)) fs.mkdirSync(destDir);
+
+      fs.rename(path.join(sourceDir, file), path.join(destDir, file), (err) => {
+        if (err) console.error("Failed move file:", err);
+        else console.log(`File ${file} success move to folder ${ext} `);
+      });
+    });
+  });
+};
+
+// script read folder
+app.readFolder = () => {
+  rl.question("Masukan Nama Folder yang ingin dibaca : ", (folderName) => {
+    const dirPath = path.join(__dirname, folderName);
+    fs.readdir(dirPath, (err, files) => {
+      if (err) return console.error("Failed Read Folder", err);
+
+      const fileDetails = files.map((file) => {
+        const filePath = path.join(dirPath, file);
+        const stats = fs.statSync(filePath);
+        return {
+          nameFile: file,
+          extensi: path.extname(file).substring(1),
+          jenisFile: ["jpg", "png"].includes(path.extname(file).substring(1))
+            ? "gambar"
+            : "text",
+          tanggalDibuat: stats.birthtime.toISOString().split("T")[0],
+          ukuranFile: `${(stats.size / 1024).toFixed(2)}kb`,
+        };
+      });
+
+      fileDetails.sort(
+        (a, b) => new Date(a.tanggalDibuat) - new Date(b.tanggalDibuat)
+      );
+      console.log(JSON.stringify(fileDetails, null, 2));
+      rl.close();
+    });
+  });
+};
+
+//  script read file
+app.readFile = () => {
+  rl.question("Masukan Nama File yang ingin dibaca : ", (fileName) => {
+    const filePath = path.join(__dirname, fileName);
+    fs.readFile(filePath, "utf-8", (err, data) => {
+      if (err) return console.error("Failed Read File", err);
+      console.log(`Isi dari file ${fileName}:\n\n${data}`);
+      rl.close();
+    });
+  });
+};
+// To Do : lanjutkan pembuatan logic disini
+
+module.exports = app;
--- index.js
+++ index.js
@@ -5,13 +5,23 @@ const args = process.argv.slice(2);
 const command = args[0];
 
 switch (command) {
-    case "make-folder":
-        app.makeFolder()
-        break;
+  case "make-folder":
+    app.makeFolder();
+    break;
+  case "make-file":
+    app.makeFile();
+    break;
+  case "ext-sorter":
+    app.extSorter();
+    break;
+  case "read-folder":
+    app.readFolder();
+    break;
+  case "read-file":
+    app.readFile();
+    break;
 
-    default:
-        throw Error("Invalid command")
-        break;
+  default:
+    throw Error("Invalid command");
+    break;
 }
- 
-
